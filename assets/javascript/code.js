b10edda395b43bddf384f49653cc1d6f2244e207
Robert Chambliss <chambliss@Roberts-MacBook-Pro.local>
2017-04-06 17:26:43 -0400

    Robert Push

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -20,20 +20,22 @@
 //==============================================================================
 
   // --> Go Button Listener.  This will receive user data and go to the giphy api to pull images.
-  $("#image-button").on("click", function() {
-    var item = $(this).attr("#image-input");
+  $("#images-button").on("click", function() {
+    
+    $("#image-display").html("");
+    
+    var item = $('#image-input').val().trim();
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
       item + "&api_key=dc6zaTOxFJmzC&limit=10";
-console.log(item);
-console.log(queryURL);
+    
     // Call to ghiphy
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
       var results = response.data;
-      console.log(results);
-      for (var i = 0; i < results.length; i++) {
+
+      for (var i = 0; i < 3; i++) {
         // Get data from ghiphy API
         var rating = results[i].rating;
         var animatedUrl = results[i].images.fixed_height.url;
@@ -42,8 +44,8 @@ console.log(queryURL);
         // Create DIV object
         var gifDiv = $("<div class='item'>");
         var p = $("<p>").text("Rating: " + rating);
-
-        var itemImage = $("<img src=" + url + " data-state='animate' alt=" + url + ">").click(function(){
+        var radioButton = $('<input type="radio" name="image" id=' + url + '/>');
+        var itemImage = $("<img src=" + url + " data-state='animate' alt=" + url + " id='image' >").click(function(){
 
         });
 
@@ -51,25 +53,28 @@ console.log(queryURL);
 
         //gifDiv.prepend(p);
         gifDiv.prepend(itemImage);
+        gifDiv.prepend(radioButton);
 
         $("#image-display").prepend(gifDiv);
       }
     });
+    $("#image-input").val("");
   });
 
   // --> Images Button Listner.  This will save user text and store in Firebase to hold the data.
-  $("#add-button").on("click", function() {
-    // Prevent form from submitting
-    event.preventDefault();
-
-    // Get input from user
-    var storyText = $("#text-area").val().trim();
-      console.log(storyText);
-    // Save input to firebase
-    database.ref().push({
-      Story : storyText
-    });
-  });
+  // $("#add-button").on("click", function() {
+  //   // Prevent form from submitting
+  //   event.preventDefault();
+  //
+  //   // Get input from user
+  //   var image = $('#image-input').val().trim();
+  //   var storyText = $("#text-area").val().trim();
+  //
+  //   // Save input to firebase
+  //   database.ref().push({
+  //     Story : storyText
+  //   });
+  // });
 
   //=============================================================================
   // DATAEBASE - Firebase
@@ -77,11 +82,41 @@ console.log(queryURL);
 
   database.ref().on("child_added", function(childSnapshot) {
 
-    // Create paragraph object
+    console.log("hello");
+    // Create HTML Objects
+    var newDiv = $("<div>");
     var newParagraph = $("<p>");
+    var newImage = $("<img>");
 
     // Add HTML
-    newParagraph.text(childSnapshot.val().storyText);
-
-    $("#recieve-text").append(newParagraph);
+    newParagraph.text(childSnapshot.val().Story);
+    newImage.text(childSnapshot.val().Image);
+    newDiv.append(newParagraph).append(newImage);
+    $("#flipbook").append(newDiv);
+    console.log(newParagraph);
   });
+
+  //=============================================================================
+  // FUNCTIONS
+  //=============================================================================
+
+  function submitForm() {
+    var userStory = $("#text-area").val().trim();
+    var userImage = $(":radio:checked").attr('id');
+    console.log(userStory + userImage);
+    var usersRef = firebase.database().ref('books');
+    var adaRef = usersRef.child('ada');
+    var adaFirstNameRef = adaRef.child('book');
+    var path = adaFirstNameRef.toString();
+    console.log(path);
+    // Save input to firebase
+    database.ref().push({
+
+      Story : userStory,
+      Image : userImage
+    });
+
+    $("#text-area").val("");
+    $("#image-display").html("");
+    $("#image-input").val("");
+  }
